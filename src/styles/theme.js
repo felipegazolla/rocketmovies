438d22711a97ff8ec515de254deba3b16b12ed1a
felipe <enggazolla@gmail.com>
2024-07-05 20:49:44 -0300

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#262529",
+    BACKGROUND_700: "#312E38",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+    BLACK: "#0D0C0F",
+
+    GRAY_100: "#E5E5E5",
+    GRAY_300: "#948F99",
+
+    RED: "#FF002E"
+  }
+}
