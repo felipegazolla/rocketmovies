bec4186ffaa3392cd953b02440535d730a484113
felipe <enggazolla@gmail.com>
2024-07-05 21:01:28 -0300

    header component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,13 @@
 import { Container } from "./styles"
 
 import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-
-      <Button title="Entrar" loading />
+      <Header />
+      <Button title="Voltar" />
     </Container>
   )
 }
