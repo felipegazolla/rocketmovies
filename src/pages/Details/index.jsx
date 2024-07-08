db7f5e5d4e22272008536a147f028676e5f4b0ec
felipe <enggazolla@gmail.com>
2024-07-08 15:21:33 -0300

    section and tags

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,11 +2,19 @@ import { Container } from "./styles"
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section>
+        <Tag title="Ficção Científica"/>
+        <Tag title="Drama"/>
+        <Tag title="Família"/>
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
