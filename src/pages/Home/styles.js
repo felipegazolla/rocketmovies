8498b0b2d058ce93897874e078a9552618dfed2f
felipe <enggazolla@gmail.com>
2024-07-29 08:53:19 -0300

    home height

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { Link } from "react-router-dom"
 
 export const App = styled.div`
-  height: 520px;
+  height: 70vh;
   position: absolute;
   overflow-y: auto;
 `
